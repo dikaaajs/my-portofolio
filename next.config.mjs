8b8b2b68346d58c6975051e77df5bcf7c19060a3
andika <zoneandika@gmail.com>
2025-04-25 20:44:32 +0700

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,3 @@
-// @ts-check
-import withSerwistInit from "@serwist/next";
-
-const withSerwist = withSerwistInit({
-  cacheOnNavigation: true,
-  swSrc: "src/sw.ts",
-  swDest: "public/sw.js",
-  reloadOnOnline: true,
-  disable: process.env.NODE_ENV === "development",
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -20,16 +9,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  transpilePackages: ["three"],
-  reactStrictMode: true,
-  experimental: {
-    reactCompiler: true,
-    // ppr: "incremental",
-  },
-  // Automatically bundle external packages in the Pages Router:
-  bundlePagesRouterDependencies: true,
-  // Opt specific packages out of bundling for both App and Pages Router:
-  // serverExternalPackages: ["package-name"],
 };
 
-export default withSerwist(nextConfig);
+export default nextConfig;
